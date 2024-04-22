48abbe59d9c1275519decaec16b1fdebf33da73f
roniandradee <ronielli.santos@sptech.school>
2024-04-22 15:42:04 -0300

    implementação: adicionando nova tela de cadastro de funcionario

--- cadastro/validaCadastro.js
+++ cadastro/validaCadastro.js
@@ -17,8 +17,8 @@ function validaCadastro() {
 
     if (nomeEmpresa == '' || email == '' || cnpj == '' || (telefone1 == '' && telefone2 == '') || senha == '' || confirmSenha == '') {
         alert('Complete todos os campos obrigatórios')
-    } else if (nomeEmpresa.length < 1) {
-        alert('Nome da empresa inválido')
+    } else if (nomeEmpresa.length < 5) {
+        span_nome.innerHTML = 'Nome inválido'
     } else if (contArroba > 1) {
         alert('O email só pode ter um @')
     } else if (email.indexOf(".") == -1) {
@@ -44,12 +44,45 @@ function validaEndereco() {
 
     if (cep.length < 8) {
         alert('CEP inválido')
-    } else if (numero == ''){
+    } else if (numero == '') {
         alert('Número inválido')
-    } else if (complemento == ''){
+    } else if (complemento == '') {
         alert('Insira um complemento')
     } else {
         window.open('../tela-login/telaLogin.html')
     }
+}
 
+function validaFuncionario() {
+    var nomeFuncionario = document.getElementById('input_nomeFuncionario').value;
+    var email = document.getElementById('input_email').value;
+    var telefone = document.getElementById('input_telefone').value;
+    var senha = document.getElementById('input_senha').value;
+    var confirmSenha = document.getElementById('input_confirmSenha').value;
+
+    for (var i = 0; i < email.length; i++) {
+        if (email[i] == "@") {
+            contArroba++
+        }
+    }
+
+    if (nomeEmpresa == '' || email == '' || cnpj == '' || (telefone1 == '' && telefone2 == '') || senha == '' || confirmSenha == '') {
+        alert('Complete todos os campos obrigatórios')
+    } else if (nomeEmpresa.length < 5) {
+        span_nome.innerHTML = 'Nome inválido'
+    } else if (contArroba > 1) {
+        alert('O email só pode ter um @')
+    } else if (email.indexOf(".") == -1) {
+        alert('Email inválido');
+    } else if (cnpj.length < 14) {
+        alert('CNPJ inválido');
+    } else if (senha.length < 6) {
+        alert('Senha precisa ter no minímo 6 caracteres');
+    } else if (senha.indexOf("!") == -1 && senha.indexOf("#") == -1 && senha.indexOf("$") == -1 && senha.indexOf('@') == -1 && senha.indexOf('%') == -1) {
+        alert('A senha precisa conter um caracter especial')
+    } else if (confirmSenha !== senha) {
+        alert('As senhas não coincidem')
+    } else {
+        window.open('./continuarCadastro.html')
+    }
 }
