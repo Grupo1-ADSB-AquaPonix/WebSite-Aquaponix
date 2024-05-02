e37b2e43a6d2c43fdc1b670909273644d8842a09
roniandradee <ronielli.santos@sptech.school>
2024-05-02 16:09:39 -0300

    refatoração: modificando tela de cadastro para API

--- cadastro/validaCadastro.js
+++ cadastro/validaCadastro.js
@@ -6,52 +6,41 @@ function validaCadastro() {
     var telefone2 = document.getElementById('input_telefone2').value;
     var senha = document.getElementById('input_senha').value;
     var confirmSenha = document.getElementById('input_confirmSenha').value;
+    var cep = document.getElementById('input_cep').value;
+    var numero = document.getElementById('input_numero').value;
+    var complemento = document.getElementById('input_complemento').value;
 
     var contArroba = 0;
 
     for (var i = 0; i < email.length; i++) {
         if (email[i] == "@") {
-            contArroba++
+            contArroba++;
         }
     }
 
-    if (razaoSocial == '' || email == '' || cnpj == '' || (telefone1 == '' && telefone2 == '') || senha == '' || confirmSenha == '') {
-        alert('Complete todos os campos obrigatórios')
-    } else if (razaoSocial.length < 5) {
-        span_nome.innerHTML = 'Nome inválido'
+    if (razaoSocial == '' || email == '' || cnpj == '' || (telefone1 == '' && telefone2 == '') || senha == '' || confirmSenha == '' || cep == '' || numero == '' || complemento == '') {
+        alert('Complete todos os campos obrigatórios');
     } else if (contArroba > 1) {
-        alert('O email só pode ter um @')
-    } else if (email.indexOf(".") == -1) {
+        alert('O email só pode ter um @');
+    } else if (email.indexOf(".") == -1 || email.indexOf("@") == -1) {
         alert('Email inválido');
     } else if (cnpj.length < 14) {
         alert('CNPJ inválido');
     } else if (senha.length < 6) {
         alert('Senha precisa ter no minímo 6 caracteres');
     } else if (senha.indexOf("!") == -1 && senha.indexOf("#") == -1 && senha.indexOf("$") == -1 && senha.indexOf('@') == -1 && senha.indexOf('%') == -1 && senha.indexOf('*') == -1) {
-        alert('A senha precisa conter um caracter especial')
+        alert('A senha precisa conter um caracter especial');
     } else if (confirmSenha !== senha) {
-        alert('As senhas não coincidem')
-    } else {
-        window.location.href = ('./continuarCadastro.html')
-    }
-}
-
-
-function validaEndereco() {
-    var cep = document.getElementById('input_cep').value;
-    var numero = document.getElementById('input_numero').value;
-    var complemento = document.getElementById('input_complemento').value;
-
-    if (cep.length < 8) {
-        alert('CEP inválido')
+        alert('As senhas não coincidem');
+    } else if (cep.length < 8) {
+        alert('CEP inválido');
     } else if (numero == '') {
-        alert('Número inválido')
+        alert('Número inválido');
     } else if (complemento == '') {
-        alert('Insira um complemento')
+        alert('Insira um complemento');
     } else {
-
-        alert('Cadastro realizado!')
-        window.location.href = ('../tela-login/telaLogin.html')
+        alert('Cadastro realizado!');
+        window.location.href = ('../tela-login/telaLogin.html');
     }
 }
 
@@ -65,19 +54,16 @@ function validaFuncionario() {
 
     for (var i = 0; i < email.length; i++) {
         if (email[i] == "@") {
-            contArroba++
+            contArroba++;
         }
     }
 
     if (nomeFuncionario == '' || email == '' || telefone == '' || senha == '' || confirmSenha == '') {
-        alert('Complete todos os campos obrigatórios')
-
+        alert('Complete todos os campos obrigatórios');
     } else if (nomeFuncionario.length < 1) {
-        alert('Nome Inválido')
-
+        alert('Nome Inválido');
     } else if (contArroba > 1) {
-        alert('O email só pode ter um @')
-
+        alert('O email só pode ter um @');
     } else if (email.indexOf(".") == -1) {
         alert('Email inválido');
 
@@ -85,14 +71,11 @@ function validaFuncionario() {
         alert('Senha precisa ter no minímo 5 caracteres');
 
     } else if (senha.indexOf("!") == -1 && senha.indexOf("#") == -1 && senha.indexOf("$") == -1 && senha.indexOf('@') == -1 && senha.indexOf('%') == -1 && senha.indexOf('*') == -1) {
-        alert('A senha precisa conter um caracter especial')
-
+        alert('A senha precisa conter um caracter especial');
     } else if (confirmSenha !== senha) {
-        alert('As senhas não coincidem')
-        
+        alert('As senhas não coincidem');
     } else {
-        
-        alert("Cadastro realizado!")
-        window.location.href = ('../dashboard/painel.html')
+        alert("Cadastro realizado!");
+        window.location.href = ('../dashboard/painel.html');
     }
 }
